f24dbc8de0bdc15ffa3eca3d95c644d5f712bffa
Jatin Rajani <mejatinrajani@gmail.com>
2026-07-07 17:42:23 +0530

    fixed the chat window dragging issue

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ from PyQt5.QtWidgets import QApplication
 from chat_window import ChatWindow
 from stealth import make_stealth
 from tray import TrayIcon
+from PyQt5.QtCore import Qt
 
 if __name__ == "__main__":
+
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     app = QApplication(sys.argv)
     chat_window = ChatWindow()
     make_stealth(chat_window)
